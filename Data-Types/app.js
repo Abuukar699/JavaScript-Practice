03c2aacbe08f731dc6a7f41a7cf0da366bf9b71c
Abuukar699 <abukarhussein93@gmail.com>
2025-03-06 03:05:26 -0800

    initial commit

---
+++ Data-Types/app.js
@@ -0,0 +1,50 @@
+ 
+
+// JavaScript Data-types are 
+
+// 1. String
+
+// 2. Number
+
+// 3. Boolean
+
+// 4. object
+
+//    5. BigInt – Used for very large numbers beyond Number.MAX_SAFE_INTEGER.
+
+//  1.String
+
+let name = "Omar";
+console.log(name)  // this is  a String 
+
+
+//2. Number
+
+let age = 25;
+let pi = 3.14;
+
+console.log(age)
+console.log(pi)
+
+
+// 3.Boolean
+
+let isStudent = true;
+console.log(isStudent)
+
+//   4. object
+
+let person = { name: "Omar", age: 25 };
+let numbers = [1, 2, 3];
+let greet = function () { console.log("Hello!"); };
+
+console.log(person);   // Daabacaa Object-ka
+console.log(numbers);  // Daabacaa Array-ga
+console.log(greet);    // Daabacaa function-ka (code-kiisa)
+greet();
+
+
+//    5. BigInt
+
+let bigNumber = 12345678901234567890n;
+console.log(bigNumber)
